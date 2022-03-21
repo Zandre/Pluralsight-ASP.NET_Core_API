b82d8e49c49fcf312ab8223b47b2e286134d69b7
Zandre Botha <botha.zandre@gmail.com>
2022-03-21 13:26:56 +0100

    Module 7 - Using EF Core in our Controllers

--- CityInfo.API/CityInfo.API/Startup.cs
+++ CityInfo.API/CityInfo.API/Startup.cs
@@ -1,13 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
 using CityInfo.API.Contexts;
 using CityInfo.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
+using Newtonsoft.Json.Serialization;
 
 namespace CityInfo.API
 {
@@ -20,8 +25,8 @@ namespace CityInfo.API
             _configuration = configuration ??
                 throw new ArgumentNullException(nameof(configuration));
         }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc()
@@ -43,16 +48,19 @@ namespace CityInfo.API
 #else
             services.AddTransient<IMailService, CloudMailService>();
 #endif
-
-            var connectionString = _configuration["connectionStrings:cityInfoDbConnectionString"];
+            var connectionString = _configuration["connectionStrings:cityInfoDBConnectionString"];
             services.AddDbContext<CityInfoContext>(o =>
             {
                 o.UseSqlServer(connectionString);
             });
+
+            services.AddScoped<ICityInfoRepository, CityInfoRepository>();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
